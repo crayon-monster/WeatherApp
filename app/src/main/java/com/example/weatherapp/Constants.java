e913ade4dc03147947d5a0516d20a7e952cc7539
Elman <elmankr30@gmail.com>
2021-01-01 00:37:34 +0400

    API key is hidden

--- app/src/main/java/com/example/weatherapp/Constants.java
+++ app/src/main/java/com/example/weatherapp/Constants.java
@@ -3,6 +3,6 @@ package com.example.weatherapp;
 public class Constants {
 
     public static final String BASE_URL = "https://api.openweathermap.org/data/2.5/";
-    public static final String API_KEY = "e87ac6deb051336befbec6450333966c";
+    public static final String API_KEY = "Insert API here";
 
 }
